1e1a79d2210f9013cb0fb5694901b55a19fb8411
renyuan <402611233@qq.com>
2015-07-05 22:42:36 +0800

    ecu handler develop

--- handler/ecu.php
+++ handler/ecu.php
@@ -19,20 +19,23 @@
 		public static function read($path='./ecu/site/laogang/ecu1234567-20280601-131706.engy'){
 			$file = fopen($path,'rb');
 			$content=fread($file,filesize($path));
+			
+			//按16位转成整数
 			$byte_array = unpack("N*",$content);
 			
 			$decode_content = '';
+			//按字节转成2进制序列，并补0
 			for($i=1;$i<=count($byte_array);$i++){
 				$_data = $byte_array[$i];
 				$_data = decbin($_data);
 				$_str_data = strval($_data);
 				$l = strlen($_str_data);
-				$plus = '';
-				if($l< BYTE_4){
-				for($k = 0;$k<(BYTE_4 - $l);$k++){
-						$plus .= '0'; 
+				$plus_zero = '';
+				if($l < BYTE_4){
+				for($k = 0;$k<(BYTE_4 - $l); $k++){
+						$plus_zero .= '0'; 
 					}
-					$_str_data = $plus.$_str_data;
+					$_str_data = $plus_zero.$_str_data;
 				}
 				$decode_content .= $_str_data;
 				
@@ -46,10 +49,13 @@
 			//文件指针的起始位置
 			$head_poiner = FILE_VERSION_SIZE;
 			
-			for($i=0;$i<$file_count;$i++){
+			for($i = 0;$i < $file_count; $i++){
 				$one_file_content = substr($decode_content,$head_poiner,FILE_CONTENT_SIZE);
+				
+				//指针前移一个完整数据字段的大小
+				$head_poiner += FILE_CONTENT_SIZE;
 				array_push($file_content,array(
-					'date'=>date('Y-m-d H:i:s',bindec(substr($one_file_content,0,32))),
+					'date'=>date('Y-m-d H:i:s',bindec(substr($one_file_content,0,BYTE_4))),
 					'tempature'=>bindec(substr($one_file_content,32,BYTE_1)),
 					'egy_save_status'=>bindec(substr($one_file_content,40,BYTE_1)),
 					'fault_code'=>substr($one_file_content,48,BYTE_1),
@@ -60,15 +66,15 @@
 						bindec(substr($one_file_content,128,BYTE_4))/100,
 						bindec(substr($one_file_content,160,BYTE_4))/100,
 					),
+					
 					'elec_power'=>array(
-						bindec(substr($one_file_content,160,BYTE_2)),
-						bindec(substr($one_file_content,176,BYTE_2)),
 						bindec(substr($one_file_content,192,BYTE_2)),
 						bindec(substr($one_file_content,208,BYTE_2)),
+						bindec(substr($one_file_content,224,BYTE_2)),
+						bindec(substr($one_file_content,240,BYTE_2)),
 					),
+					
 					'temperature'=>array(
-						bindec(substr($one_file_content,225,BYTE_2_UNSIGN))/100,
-						bindec(substr($one_file_content,241,BYTE_2_UNSIGN))/100,
 						bindec(substr($one_file_content,257,BYTE_2_UNSIGN))/100,
 						bindec(substr($one_file_content,273,BYTE_2_UNSIGN))/100,
 						bindec(substr($one_file_content,289,BYTE_2_UNSIGN))/100,
@@ -77,13 +83,22 @@
 						bindec(substr($one_file_content,337,BYTE_2_UNSIGN))/100,
 						bindec(substr($one_file_content,353,BYTE_2_UNSIGN))/100,
 						bindec(substr($one_file_content,369,BYTE_2_UNSIGN))/100,
+						bindec(substr($one_file_content,385,BYTE_2_UNSIGN))/100,
+						bindec(substr($one_file_content,401,BYTE_2_UNSIGN))/100,
 					),
+					
 					'humidity'=>array(
-						bindec(substr($one_file_content,384,BYTE_1)),
-						bindec(substr($one_file_content,392,BYTE_1)),
+						bindec(substr($one_file_content,416,BYTE_1)),
+						bindec(substr($one_file_content,424,BYTE_1)),
+					),
+					
+					'reserved'=>array(
+						bindec(substr($one_file_content,432,BYTE_1)),
+						bindec(substr($one_file_content,440,BYTE_1)),
 					)
+					
 				));
-				$head_poiner += FILE_CONTENT_SIZE;
+				
 			}
 			
 			return array(
