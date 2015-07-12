19e13102e553f1cfa3d34d8508606d3077848a1d
renyuan <402611233@qq.com>
2015-07-12 18:38:59 +0800

    some bugfix

--- handler/weather.php
+++ handler/weather.php
@@ -3,7 +3,7 @@ class WeatherHandler {
     //显示所有天气
     public static function show(){
          $dao =  new PowerWeatherMySqlDAO();        
-         $list=$dao->queryAllOrderBy('create_time desc');        
+         $list=$dao->queryAllOrderBy('create_time desc');       	 
          if($list){
              foreach($list as $k=>$v){
                 $v->createTime=date('Y-m-d h:i',$v->createTime);
@@ -15,6 +15,7 @@ class WeatherHandler {
     }
       //获取地区天气信息
      public function get_weather($district='上海'){        
+		
        $date=  date("Ymdhis");
        $str="area".$district."showapi_appid2630showapi_timestamp".$date."ab14ae8e4d3d467b9adf0443d2d8a486";
        $sign=  md5($str);
@@ -29,25 +30,22 @@ class WeatherHandler {
             $weather['temperature_low']=$data['showapi_res_body']['f1']['night_air_temperature'];
             $weather['wind']=  str_replace('级', '', $data['showapi_res_body']['now']['wind_power']);
        }      
-//               echo "<pre>";print_r($weather);echo "</pre>";      
        return $weather;
         
      }
     //添加一条天气
     public  function add($area,$weatherinfo){     	
-	$weather = new PowerWeather();
-	$weather->province=$area['province'];
+		$weather = new PowerWeather();
+		$weather->province=$area['province'];
         $weather->city=$area['city'];
         $weather->district=$area['district'];
         $weather->weather=$weatherinfo['weather'];
         $weather->temperatureHigh=$weatherinfo['temperature_high'];
         $weather->temperatureLow=$weatherinfo['temperature_low'];
         $weather->wind=$weatherinfo['wind'];	
-	$weather->createTime = time();
+		$weather->createTime = time();
         $dao =  new PowerWeatherMySqlDAO(); 
-	$dao->insert($weather);
-         
- 
+		$dao->insert($weather);
     }
         //获取所有基站所在地区的名称添加天气
      public static function add_weather(){
@@ -55,11 +53,14 @@ class WeatherHandler {
          if(!$distrct){
              $distrct=array(array('province'=>'上海','city'=>'上海','district'=>'上海'));            
          }
+		 
+		 $distrct=array(array('province'=>'上海','city'=>'上海','district'=>'上海'));
          foreach($distrct as $v){
+		 
             $weather=self::get_weather($v['district']);
             if($weather){
                 self::add($v, $weather);
-                return true;
+                return $weather;
             }else{
                 return false;  
             }
