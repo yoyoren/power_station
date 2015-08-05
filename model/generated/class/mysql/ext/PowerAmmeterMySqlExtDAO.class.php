7f4d8e472ff35476f65aa38b5198e509583130cf
gaoqiang198744 <306757051@qq.com>
2015-08-05 21:47:56 +0800

    修改e值bug，增加抄表历史查询

--- model/generated/class/mysql/ext/PowerAmmeterMySqlExtDAO.class.php
+++ model/generated/class/mysql/ext/PowerAmmeterMySqlExtDAO.class.php
@@ -6,18 +6,36 @@
  * @date: 2015-06-25 11:03
  */
 class PowerAmmeterMySqlExtDAO extends PowerAmmeterMySqlDAO{
-        public function queryAndPage($start,$end){
-            $sql = 'SELECT * FROM power_ammeter order by ammeter_id desc limit '.$start.','.$end;		
+       //非搜索查询全部
+        public function queryAndPage($start,$end,$where=''){           
+            $user_id=$_COOKIE['user_id'];          
+            $where=" where station_id in("."SELECT s.station_id FROM power_account_access_project p,power_base_station s where p.project_id=s.project_id and account_id=".$user_id.")";
+            //查询是否是管理员
+            $a=DAOFactory::getPowerAccountDAO()->load($user_id);
+            if($a->accoutType==1) $where="";
+            $sql = "SELECT * FROM power_ammeter ".$where." order by ammeter_id desc limit ".$start.",".$end;		
             $sqlQuery = new SqlQuery($sql);
             return $this->getList($sqlQuery);
 	}
         public function queryStation($station){
-            $user_id=$_COOKIE['user_id'];
-            $sql = "SELECT s.station_id FROM power_account_access_project p,power_base_station s where p.project_id=s.project_id and account_id=".$user_id." and s.station_name='".$station."'";
+            $user_id=$_COOKIE['user_id'];                  
+            //查询是否是管理员
+            $a=DAOFactory::getPowerAccountDAO()->load($user_id);
+            if($a->accoutType==2){//普通用户
+                $sql = "SELECT s.station_id FROM power_account_access_project p,power_base_station s where p.project_id=s.project_id and account_id=".$user_id." and s.station_name='".$station."'"; 
+            }else{
+                $sql = "SELECT s.station_id FROM power_base_station s where  s.station_name='".$station."'";
+            } 
             $sqlQuery = new SqlQuery($sql);
             $result=$this->execute($sqlQuery);
             return $result[0][0];
 	}
+        public function search($start,$end,$sql=''){           
+            $sql = "SELECT * FROM power_ammeter ".$sql." order by ammeter_id desc limit ".$start.",".$end;		
+            $sqlQuery = new SqlQuery($sql);
+            return $this->getList($sqlQuery);
+	}
+        
 	
 }
 ?>
